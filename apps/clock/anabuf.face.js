d23f6abdf49b9e038de1dcaabe040b10df0097b7
jeffmer <jeffmer@users.noreply.github.com>
2020-10-26 14:58:51 +0000

    back to unbuffedred ana clock face

--- apps/clock/anabuf.face.js
+++ apps/clock/anabuf.face.js
@@ -4,61 +4,70 @@
 
     const p = Math.PI/2;
     const PRad = Math.PI/180;
+    //var pal2color = new Uint16Array([0x0000,0xF800,0x07ff,0xffff],0,4);
+    var buf = Graphics.createArrayBuffer(240,212,1,{msb:true});
+    function flip() {
+      g.setColor(1,1,1);
+      g.drawImage({width:240,height:212,bpp:1,buffer:buf.buffer},0,25);
+    }
+
 
     function seconds(angle, r) {
         const a = angle*PRad;
         const x = 120+Math.sin(a)*r;
-        const y = 134-Math.cos(a)*r;
+        const y = 106-Math.cos(a)*r;
         if (angle % 90 == 0) {
-            g.setColor(0,1,1);
-            g.fillRect(x-6,y-6,x+6,y+6);
+            buf.setColor(1);
+            buf.fillRect(x-6,y-6,x+6,y+6);
         } else if (angle % 30 == 0){
-            g.setColor(1,1,1);
-            g.fillRect(x-4,y-4,x+4,y+4);
+            buf.setColor(1);
+            buf.fillRect(x-4,y-4,x+4,y+4);
         } else {
-            g.setColor(1,1,1);
-            g.fillRect(x-1,y-1,x+1,y+1);
+            buf.setColor(1,1,1);
+            buf.fillRect(x-1,y-1,x+1,y+1);
         }
     }
 
     function hand(angle, r1,r2, r3) {
         const a = angle*PRad;
-        g.fillPoly([
+        buf.fillPoly([
             120+Math.sin(a)*r1,
-            134-Math.cos(a)*r1,
+            106-Math.cos(a)*r1,
             120+Math.sin(a+p)*r3,
-            134-Math.cos(a+p)*r3,
+            106-Math.cos(a+p)*r3,
             120+Math.sin(a)*r2,
-            134-Math.cos(a)*r2,
+            106-Math.cos(a)*r2,
             120+Math.sin(a-p)*r3,
-            134-Math.cos(a-p)*r3]);
+            106-Math.cos(a-p)*r3]);
     }
 
     var minuteDate;
     var secondDate;
 
     function onSecond() {
-        g.setColor(0,0,0);
+        buf.setColor(0);
         hand(360*secondDate.getSeconds()/60, -5, 90, 3);
         if (secondDate.getSeconds() === 0) {
             hand(360*(minuteDate.getHours() + (minuteDate.getMinutes()/60))/12, -16, 60, 7);
             hand(360*minuteDate.getMinutes()/60, -16, 86, 7);
             minuteDate = new Date();
         }
-        g.setColor(1,1,1);
+        buf.setColor(1);
         hand(360*(minuteDate.getHours() + (minuteDate.getMinutes()/60))/12, -16, 60, 7);
         hand(360*minuteDate.getMinutes()/60, -16, 86, 7);
-        g.setColor(0,1,1);
+        buf.setColor(1);
         secondDate = new Date();
         hand(360*secondDate.getSeconds()/60, -5, 90, 3);
-        g.setColor(0,0,0);
-        g.fillCircle(120,134,2);
+        buf.setColor(0);
+        buf.fillCircle(120,106,2);
+        flip();
     }
 
     function drawAll() {
         secondDate = minuteDate = new Date();
         // draw seconds
-        g.setColor(1,1,1);
+        buf.clear();
+        buf.setColor(1);
         for (let i=0;i<60;i++)
             seconds(360*i/60, 100);
         onSecond();

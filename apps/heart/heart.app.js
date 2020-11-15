6b6a964fd128822411b1ace06251f51f0a7fa62a
jeffmer <jeffmer@users.noreply.github.com>
2020-11-15 10:16:13 +0000

    update heart

--- apps/heart/heart.app.js
+++ apps/heart/heart.app.js
@@ -5,20 +5,23 @@
 /*
 var correlator = E.compiledC(`
 // void put(int)
-// int conf(void)
+// int Cmax(void)
+// int Cmin(void)
 // int bpm(void)
 
 __attribute__((section(".text"))) const int NSLOT = 128;
 __attribute__((section(".text"))) char buffer[NSLOT];
 __attribute__((section(".text"))) int next = 0;
-__attribute__((section(".text"))) int confidence = 0;
+__attribute__((section(".text"))) int correlationMin = 0;
+__attribute__((section(".text"))) int correlationMax = 0;
 
 void put(int v){
     buffer[next]=v;
     next = (next+1)%NSLOT;
 }
 
-int conf() {return confidence;}
+int Cmax() {return correlationMax;}
+int Cmin() {return correlationMin;}
 
 int bpm() {
     const int CMIN = 7; // 60000/(200bpm * 40ms)
@@ -40,18 +43,19 @@ int bpm() {
         if (s<minCorr) {minCorr=s; minIdx=c;}
         if (s>maxCorr) maxCorr = s;
     }
-    confidence = 120 - (minCorr/600);
-    confidence = confidence<0?0:confidence>100?100:confidence;
+    correlationMin = minCorr;
+    correlationMax = maxCorr;
     return  minIdx==0?0:(60000/(minIdx*40));
 }
 `);
 */
 var correlator = (function(){
-  var bin=atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC3p8EMvTX1EBybV+ACAACCo8QcEb/AAQwAsIkZBRti/BPGAAk/wWwwAJwXrAQ6e+ASQBesCDp74BODO6wkOAfEBCR5JCeoBAQApAvEBCRtKuL8B8f8xCeoCAry/YfB/AQExACq+vwLx/zJi8H8CATK88QEMDvsOd9fRn0K4vzBGBvEBBri/O0YlLgTx/zTD0QtKk/vy8wxKeDNkK6i/ZCN6RCPq43PC+IQwKLEoI1hDTvZgI5P78PC96PCDfwAAgKj9//9u////1P7//wJLe0TT+IQAcEcAv6b+//8JSXlEC2jKGBBxWhwFSxNAACu+vwPx/zNj8H8DATMLYHBHAL9/AACAlv7//w==");
+  var bin=atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAt6fBHKk19RAAg1fgAgAcmqPEHAYZGb/AATAApC0ZCRti/AfGAA0/wWwkAJK8Yl/gEoO8YP3nH6woHAvEBChtKCuoCAgAqA/EBChhLuL8C8f8yCuoDA7y/YvB/AgEyACu+vwPx/zNj8H8DATO58QEJB/sHRNrRZEW8vzBGpEYBNqZFuL+mRiUuAfH/McXRCUt7RMP4hMDD+IjgKLEoI1hDTvZgI5P78PC96PCHfwAAgGr////g/v//Akt7RNP4hABwRwC/tv7//wJLe0TT+IgAcEcAv6b+//8JSXlEC2jKGBBxWhwFSxNAACu+vwPx/zNj8H8DATMLYHBHAL9/AACAlv7//w==");
   return {
     put:E.nativeCall(357, "void(int)", bin),
-    conf:E.nativeCall(341, "int(void)", bin),
-    bpm:E.nativeCall(137, "int(void)", bin),
+    Cmax:E.nativeCall(341, "int(void)", bin),
+    Cmin:E.nativeCall(325, "int(void)", bin),
+    bpm:E.nativeCall(141, "int(void)", bin),
   };
 })();
 /*
@@ -332,9 +336,7 @@ function doread(){
 
 function showBPM(){
   var bpm = correlator.bpm();
-  var conf = correlator.conf();
   g.setColor(0xFFE0).drawString("BPM: "+bpm+" ",10,0,true);
-  g.drawString("CNF: "+conf+"%  ",10,20,true);
 }
 
 function startMeasure() {
